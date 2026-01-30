a82d4d5c01cc51812e6d1b6b2ed4f1e2e630b6eb
JulienC7 <julien.clavier95@gmail.com>
2026-01-30 10:36:53 +0100

    Mise en place des fonts

---
+++ julienclavier/fonts/fonts.tsx
@@ -0,0 +1,19 @@
+import { Saira } from "next/font/google";
+import { Cinzel } from "next/font/google";
+
+ 
+export const Saira300 = Saira({
+  subsets: ["latin"],
+  weight: "300",
+});
+
+export const Cinzel400 = Cinzel({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const Cinzel700 = Cinzel({
+  subsets: ["latin"],
+  weight: "700",
+});
+
